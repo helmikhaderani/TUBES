6f3513b53b368b1e42a5efa36b2139ff2b4c4816
Mualfatih <82765813+Mualfatih@users.noreply.github.com>
2021-04-18 21:29:44 +0700

    Delete f08-f09.py

--- f08-f09.py
+++
@@ -1,8 +0,0 @@
-def peminjaman_gadgets():
-    
-    gadgets = []
-
-    with open(gadget.csv , mode="r") as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            gadgets.append(row)
